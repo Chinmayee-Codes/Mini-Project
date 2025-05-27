3eedf1a41e8cd3688ba231600fdb6d6b840e25e5
Chinmayee-Codes <m24de3026@iitj.ac.in>
2025-05-27 20:44:35 +0530

    band generator

---
+++ CLI Band Name Generator/code.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Generator")
+city = input("What is the name of the city you grew up in?\n")
+pet = input("What is your pet's name?\n")
+band = city + ' ' + pet
+print(f"Your band name could be {band}")
