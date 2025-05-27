23d228dbbf5d530f48fbe8ee99b79a6ad60b9834
Chinmayee BM <162834175+Chinmayee-Codes@users.noreply.github.com>
2025-05-27 20:47:51 +0530

    Update code.py

--- CLI Band Name Generator/code.py
+++ CLI Band Name Generator/code.py
@@ -1,5 +1,5 @@
 print("Welcome to the Band Generator")
-city = input("What is the name of the city you grew up in?\n")
-pet = input("What is your pet's name?\n")
-band = city + ' ' + pet
-print(f"Your band name could be {band}")
+City = input("What is the name of the city you grew up in?\n")
+Pet = input("What is your pet's name?\n")
+Band = City + ' ' + Pet
+print(f"Your band name could be {Band}")
